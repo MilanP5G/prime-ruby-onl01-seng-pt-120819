9a71ff78621036538a43de408f7559f3de31162c
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-18 15:11:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,8 @@
 def prime?(number)
   
   return false if number < 2
-  (2..number).each do |i| 
-    return false if number % i == 0
- 
+  (2..number).each {|i| return false if number % i == 0}
+  true
    
 end 
     
